29c218f6c90c9787e265986e1e64bdceb81126de
shefalipai <shefalikpai@gmail.com>
2021-09-26 13:10:01 -0400

    Progress on EX04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,21 @@
 """List utility functions."""
 
-__author__ = "123456789"
-
+__author__ = "730466264"
 
 # TODO: Implement your functions here.
+
+
+def main() -> None: 
+    list_1: list[int] = [1, 2, 3]
+    print(1)
+    print(all(list_1))
+
+
+def all(x: list[int]) -> bool:
+    print("enter all")
+    return False
+
+
+if __name__ == "main()":
+    print("utils")
+    main()
