cd0f56ea4f59453f648e11329dc05feb75ea2fd6
shefalipai <shefalikpai@gmail.com>
2021-10-03 16:50:40 -0400

    EX05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,74 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730466264"
 
 # Define your functions below
+from typing import List
+
+
+def main() -> None: 
+    list_1: List[int] = [1, 3, 5]
+    list_2: List[int] = [1]
+    a_list: List[int] = []
+    number_1: int = 1
+    number_2: int = 3
+    print(only_evens(list_1))
+    print(sub(a_list, number_1, number_2))
+    print(concat(list_1, list_2))
+
+
+def only_evens(list_1: List[int]) -> List[int]:
+    i: int = 0
+    even_list: List[int] = []
+
+    while i < len(list_1):
+        if list_1[i] % 2 == 0:
+            even_list.append(list_1[i])
+        i += 1
+    return even_list
+
+
+def sub(a_list: List[int], number_1: int, number_2: int) -> List[int]:
+    new_list: List[int] = []
+    empty_list: List[int] = []
+    # new_list.append(a_list[number_1])
+    # new_list.append(a_list[number_2 - 1])
+
+    if len(a_list) == 0: 
+        return empty_list
+    else: 
+        if number_1 > len(a_list) - 1: 
+            return empty_list
+        else: 
+            if number_2 <= 0: 
+                return empty_list
+
+    if number_1 < 0:
+        new_list.append(a_list[0])
+    if number_2 > len(a_list):
+        new_list.append(a_list[len(a_list) - 1])
+    else: 
+        new_list.append(a_list[number_1])
+        new_list.append(a_list[number_2 - 1])
+    
+    return new_list
+
+
+def concat(list_1: List[int], list_2: List[int]) -> List[int]:
+    i: int = 0
+    concat_list: List[int] = []
+
+    while i < len(list_1):
+        concat_list.append(list_1[i])
+        i += 1
+    
+    i = 0
+    while i < len(list_2):
+        concat_list.append(list_2[i])
+        i += 1
+
+    return concat_list
+
+
+if __name__ == "__main__":
+    main()
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,70 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730466264"
+
+from typing import List
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_even_list() -> None:
+    input_list: List[int] = [1, 2, 3, 4, 5, 6]
+    even_list: List[int] = [2, 4, 6]
+    assert only_evens(input_list) == even_list
+
+
+def test_only_evens_no_evens() -> None: 
+    input_list: List[int] = [1, 3, 5, 7, 9]
+    even_list: List[int] = []
+    assert only_evens(input_list) == even_list
+
+
+def test_only_evens_input_empty() -> None: 
+    input_list: List[int] = []
+    even_list: List[int] = []
+    assert only_evens(input_list) == even_list
+
+
+def test_sub_normal_case() -> None: 
+    input_list: List[int] = [1, 2, 3, 4, 5]
+    sub_list: List[int] = [2, 3]
+    number_1: int = 1
+    number_2: int = 3
+    assert sub(input_list, number_1, number_2) == sub_list
+
+
+def test_sub_input_empty() -> None: 
+    input_list: List[int] = []
+    sub_list: List[int] = []
+    number_1: int = 1
+    number_2: int = 3
+    assert sub(input_list, number_1, number_2) == sub_list
+
+
+def test_sub_large_boundaries() -> None: 
+    input_list: List[int] = [1, 2, 3, 4, 5]
+    sub_list: List[int] = [1, 5]
+    number_1: int = -1
+    number_2: int = 6
+    assert sub(input_list, number_1, number_2) == sub_list
+
+
+def test_concat_empty_list() -> None: 
+    input_list: List[int] = []
+    input_list_2: List[int] = []
+    concat_list: List[int] = []
+    assert concat(input_list, input_list_2) == concat_list
+
+
+def test_concat_normal_case() -> None: 
+    input_list: List[int] = [1, 2, 3, 4, 5]
+    input_list_2: List[int] = [1, 2, 3]
+    concat_list: List[int] = [1, 2, 3, 4, 5, 1, 2, 3]
+    assert concat(input_list, input_list_2) == concat_list
+
+
+def test_concat_empty_first_list() -> None: 
+    input_list: List[int] = []
+    input_list_2: List[int] = [1, 2, 3]
+    concat_list: List[int] = [1, 2, 3]
+    assert concat(input_list, input_list_2) == concat_list
